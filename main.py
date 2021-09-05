db904ab4adafe09eab6d91022af3862e21d5c5e0
Влад <skrynnikovsky@gmail.com>
2021-09-05 16:07:19 +0300

    Minor changes

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
+'''
+Roll and dice game
+'''
 import tkinter as tk
 import random as rd
 
-window = tk.Tk()
-edge = int(window.winfo_screenheight()/2)
-window.geometry(str(edge) + 'x' + str(edge))
-window.title('Dicer')
 
-dice = ['⚀', '⚁', '⚂', '⚃', '⚄', '⚅']
+def main():
+    '''
+    Main game loop
+    '''
 
-roll = tk.Label(window, text = rd.choice(dice), font = ('Arial', int(edge/2)))
-roll.pack(expand = True)
+    def rolling_dice():
+        roll.configure(text=rd.choice(dice))
+        roll.pack()
 
-def rolling_dice():
-  roll.configure(text = rd.choice(dice))
-  roll.pack
+    window = tk.Tk()
+    edge = int(window.winfo_screenheight()/2)
+    window.geometry(str(edge) + 'x' + str(edge))
+    window.title('Dicer')
+
+    dice = ['⚀', '⚁', '⚂', '⚃', '⚄', '⚅']
+
+    roll = tk.Label(window,
+                    text=rd.choice(dice),
+                    font=('Arial', int(edge/2)))
+
+    play = tk.Button(window,
+                     text='Roll',
+                     command=rolling_dice)
+
+    close = tk.Button(window,
+                      text='Close',
+                      command=window.destroy)
+
+    roll.pack(expand=True)
+    play.pack(fill='x')
+    close.pack(fill='x')
+
+    window.mainloop()
 
 
-play = tk.Button(window, text = 'Roll', command = rolling_dice)
-play.pack(fill = 'x')
-
-close = tk.Button(window, text = 'Close', command = window.destroy)
-close.pack(fill = 'x')
-
-window.mainloop()
-
+if __name__ == '__main__':
+    main()
